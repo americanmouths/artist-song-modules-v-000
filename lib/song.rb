cf62e8c2a005c46a0eb8fd347551f6281b355d4e
Kayla Hansen <shiverr@gmail.com>
2017-10-23 03:49:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,8 @@ require 'pry'
 require_relative '../config/environment.rb'
 
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
